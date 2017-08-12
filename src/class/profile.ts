c369d2aaf513f082379740c11d1ea091a566259b
neo <antonio.fernandez.888@gmail.com>
2017-08-13 01:34:48 +0700

    add class

---
+++ src/class/profile.ts
@@ -0,0 +1,33 @@
+export class General {   
+     custcashtag:string;
+     custname:string;
+     custaccnumber:string;
+     custbalance:number;
+     merchantcashtag:string;
+     merchantname:string;
+     merchantaccnumber:string;
+     merchantbalance:number;
+     trxid:string;
+     trxname:string;
+     trxcategory:string;
+     trxdate:Date;
+     trxamount:number;
+     trxstatus:string;   
+}
+
+				
+
+ 
+
+
+
+
+
+						
+						
+
+
+
+
+
+		
