b550f103c3483473c29e6799f62d68197990593a
neo <antonio.fernandez.888@gmail.com>
2017-08-13 03:01:12 +0700

    update confirmation

--- src/pages/confirmation/confirmation.ts
+++ src/pages/confirmation/confirmation.ts
@@ -1,22 +1,55 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import {  HomeService} from '../../shared/home.service';
 
-
+import {  General} from '../../class/profile';
 @IonicPage()
 @Component({
   selector: 'page-confirmation',
   templateUrl: 'confirmation.html',
+  providers: [HomeService],
 })
 export class ConfirmationPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App) {
+  public buyer: string;
+   public seller: string;
+  public listConf: Array<General> = new Array<General>();
+  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App, private service: HomeService) {
+     
+    // this.seller = "$seller";
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConfirmationPage');
+  this.paymentConfirmation("$buyer");
+  
+      // this.buyer = this.navParams.get('payerCashTag');
+      // this.seller = this.navParams.get('requesterCashTag');
+    }
+  
+    
+    paymentConfirmation(cashtag: any) {
+    return new Promise((resolve, reject) => {
+      resolve(this.service.paymentConfirmation(cashtag).subscribe(
+      data => {
+        data.forEach(e => {
+         let g = new General();
+          g.custname = e.payerName;
+          g.merchantname = e.requesterName;
+          g.trxamount = e.amount;
+          g.trxid = e.referenceNo.toString().substring(0,6);
+          this.listConf.push(g);
+        });
+          this.buyer = data.requesterCashTag;
+          this.seller = data.payerCashTag;
+      }, 
+      errors => {
+
+      }));
+    });
   }
-  submitRequest(){
+
+    submitRequest(){
     this.app.getRootNav().setRoot(HomePage, {
       isMerchant: false,
       amount : '1.000 IDR',
